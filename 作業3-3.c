303481aa06d21d6b564504208dd933aa1cf819ef
benton5566 <108136228+benton5566@users.noreply.github.com>
2023-05-21 17:54:36 +0800

    Add files via upload

--- 作業3-3.c
+++ 作業3-3.c
@@ -1,26 +1,29 @@
 #include <stdio.h>
 
 int main() {
-    int n=5, sum = 0;
+    int n=10, sum = 0;
 
-    printf("請輸入一個正整數: ");
+    printf("enter a positive integer:\n\n");
     //scanf("%d", &n);
+    printf("calculation process:\n\n");
     for(int k=1;k<=n;k++){
+    printf("%d!=",k);
     sum=sum+ttsum(k);
+    printf("%d\n",ttsum(k));
     }
-    printf("\n 1! + 2! + … + n!的值: %d %d \n", n, sum);
-
+    printf("\nsum of 1!+2!+...+n!: %d %d \n", n, sum);
     return 0;
 }
 
 int ttsum(int n){
 
-    int sum=1;
+    int sum=1,k;
 
     for (int i = 1; i <= n; i++) {
-            printf("\n %d,%d . ", i, sum);
-        sum = sum*i;
+            //printf("\n %d,%d\n . ", i, sum);
+        sum=sum*i;
     }
+
 
     return sum;
 }
